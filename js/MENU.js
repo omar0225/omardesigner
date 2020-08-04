a5ef0a05c047050b5a912bbc41eb4fa14abfb72d
omar0225 <69174785+omar0225@users.noreply.github.com>
2020-08-03 19:59:59 -0500

    Add files via upload

---
+++ js/MENU.js
@@ -0,0 +1,15 @@
+// JavaScript Document
+$(document).ready(main);
+
+var contador = 1 ;
+
+function main()
+{
+ $('.menu_bar').click(function() 
+                     {
+    $('nav').toggle();                  
+     
+                      
+     }) ;  
+    
+};
